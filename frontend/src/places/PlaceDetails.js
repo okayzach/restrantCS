e5bea2f730128dea5152578409962a1a6221b05b
okayzach <znortyy@gmail.com>
2022-08-17 21:39:49 -0400

    finished hw

--- frontend/src/places/PlaceDetails.js
+++ frontend/src/places/PlaceDetails.js
@@ -102,6 +102,21 @@ function PlaceDetails() {
 		})
 	}
 
+	let placeActions = null
+
+	if (currentUser?.role === 'admin') {
+	placeActions = (
+		<>
+			<a className="btn btn-warning" onClick={editPlace}>
+				Edit
+			</a>
+			<button type="submit" className="btn btn-danger" onClick={deletePlace}>
+				Delete
+			</button>
+		</>
+	)
+	}
+
 
 	return (
 		<main>
@@ -129,9 +144,7 @@ function PlaceDetails() {
 						Serving {place.cuisines}.
 					</h4>
 					<br />
-					<a className="btn btn-warning" onClick={editPlace}>
-						Edit
-					</a>{` `}
+					{placeActions}
 					<button type="submit" className="btn btn-danger" onClick={deletePlace}>
 						Delete
 					</button>
